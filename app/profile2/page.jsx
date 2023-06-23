1c880c4520c6212d3f13b0b30569759551c39be2
foysalsat <foysal20599@gmail.com>
2023-06-23 15:38:57 +0600

    First Commit

---
+++ app/profile2/page.jsx
@@ -0,0 +1,13 @@
+import "font-awesome/css/font-awesome.min.css";
+import Image from "next/image";
+const Profile2 = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="card">
+        <Image height={300} width={300} src="/images/foysal.png" alt="Foysal Ahmed Image" />
+      </div>
+    </div>
+  );
+};
+
+export default Profile2;
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import image from "/public/images/contact/image1.webp";
+import image1 from "/public/images/contact/image2.webp";
+import image2 from "/public/images/contact/image3.jpeg";
+import image3 from "/public/images/contact/image4.jpg";
+import image4 from "/public/images/contact/image5.jpg";
+import image5 from "/public/images/contact/image6.jpeg";
+import image6 from "/public/images/contact/image7.jpg";
+
+export { image, image1, image2, image3, image4, image5, image6 };
